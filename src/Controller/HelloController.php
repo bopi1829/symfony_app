240db7e9a838221b512760abdc0acb4bba4cc0c8
bopi1829 <p.boucharlat@agencepbweb.com>
2020-11-05 11:24:49 +0100

    end of training

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Service\Greeter;
+
+class HelloController extends AbstractController {
+    
+    /**
+     * @Route("hello")
+     */    
+    function hello(Greeter $greeter) {
+        $message = $greeter->greet();
+        return new Response($message);
+    }
+    
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends AbstractController {
+
+    /**
+     * @Route("/user")
+     */
+    function createUserForm(Request $request) {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $userInfos = $form->getData();
+            //var_dump($userInfos); die;
+                        
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($userInfos);
+            $entityManager->flush();
+
+            return new Response('Formulaire validé.');
+        }
+
+        return $this->render('form.html.twig', ['userForm' => $form->createView()]);      
+
+    }
+
+}
